4eaba959bc29d446377d8dac402eb9a810000816
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2022-12-02 23:14:16 +0530

    solved day1 part 1

--- day0/main.go
+++ day0/main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 
-	"../common"
+	"github.com/ashishjh-bst/aoc2022/common"
 )
 
 func main() {
 	args := os.Args[1:]
-	inputFilePath := args[1]
+	inputFilePath := args[0]
 	input := common.ReadFileInput(inputFilePath)
-	fmt.Printf(input)
+	foodItems := strings.Split(input, "\n")
+	maxCal := 0
+	currElfItemSum := 0
+	for _, item := range foodItems {
+		if item == "" {
+			currElfItemSum = 0
+		} else {
+			cals, err := strconv.Atoi(item)
+			if err != nil {
+				log.Fatal("Couldn't convert string to int for %s, check input")
+			}
+			currElfItemSum += cals
+		}
+		if maxCal < currElfItemSum {
+			maxCal = currElfItemSum
+		}
+	}
+	fmt.Printf("Max Calories %d \n", maxCal)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ashishjh-bst/aoc2022
 
 go 1.17
+
+replace github.com/botlabs-gg/yagpdb/v2 => ../yagpdb
