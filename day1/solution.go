336af08b668123bf60e33a12ff2f4e80b19df968
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2022-12-06 16:36:33 +0530

    dryness

--- day1/solution.go
+++ day1/solution.go
@@ -8,27 +8,16 @@ import (
 )
 
 func Part1(input *string) string {
-	foodItems := strings.Split(*input, "\n")
-	maxCal := 0
-	currElfItemSum := 0
-	for _, item := range foodItems {
-		if item == "" {
-			currElfItemSum = 0
-		} else {
-			cals, err := strconv.Atoi(item)
-			if err != nil {
-				log.Fatalf("Couldn't convert string to int for %s, check input", item)
-			}
-			currElfItemSum += cals
-		}
-		if maxCal < currElfItemSum {
-			maxCal = currElfItemSum
-		}
-	}
-	return strconv.Itoa(maxCal)
+	elfCals := calSum(input)
+	return strconv.Itoa((*elfCals)[0])
 }
 
 func Part2(input *string) string {
+	elfCals := calSum(input)
+	return strconv.Itoa((*elfCals)[0] + (*elfCals)[1] + (*elfCals)[2])
+}
+
+func calSum(input *string) *[]int {
 	foodItems := strings.Split(*input, "\n")
 	elfCals := []int{}
 	currElfItemSum := 0
@@ -48,5 +37,5 @@ func Part2(input *string) string {
 	sort.Slice(elfCals, func(i, j int) bool {
 		return elfCals[i] > elfCals[j]
 	})
-	return strconv.Itoa(elfCals[0] + elfCals[1] + elfCals[2])
+	return &elfCals
 }
