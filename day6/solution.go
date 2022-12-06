c6d3e18ad730bed83350d54ed5e25291c59d85b0
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2022-12-06 16:29:28 +0530

    dryness

--- day6/solution.go
+++ day6/solution.go
@@ -6,27 +6,15 @@ import (
 
 func Part1(input *string) string {
 	markerSize := 4
-	for i := 0; i < len(*input); i++ {
-		if i+(markerSize-1) >= len(*input) {
-			return "0"
-		}
-		marker := make(map[rune]int, 0)
-		for j := 0; j < markerSize; j++ {
-			char := rune((*input)[i+j])
-			marker[char]++
-			if marker[char] > 1 {
-				break
-			}
-		}
-		if len(marker) == markerSize {
-			return strconv.Itoa(i + markerSize)
-		}
-	}
-	return "0"
+	return parseInput(input, markerSize)
 }
 
 func Part2(input *string) string {
 	markerSize := 14
+	return parseInput(input, markerSize)
+}
+
+func parseInput(input *string, markerSize int) string {
 	for i := 0; i < len(*input); i++ {
 		if i+markerSize-1 >= len(*input) {
 			return "0"
