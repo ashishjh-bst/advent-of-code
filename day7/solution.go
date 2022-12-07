41543ea88d4e27e452daa64e3f9f321986c955dc
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2022-12-08 04:29:30 +0530

    solved day 2

--- day7/solution.go
+++ day7/solution.go
@@ -2,6 +2,7 @@ package day7
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -22,7 +23,16 @@ func Part1(input *string) string {
 }
 
 func Part2(input *string) string {
-	return ""
+	lines := strings.Split(*input, "\n")
+	dir := &Directory{size: 0, name: "/", directories: make(map[string]*Directory, 0)}
+	fileSystemParser(lines, dir)
+	spaceNeeded := 30000000 - (70000000 - dir.size)
+	dirs := getAllEligibleSubDir(dir, spaceNeeded)
+	sort.Slice(dirs, func(i, j int) bool {
+		return dirs[i].size > dirs[j].size
+	})
+	spaceFreed := strconv.Itoa(dirs[len(dirs)-1].size)
+	return spaceFreed
 }
 
 func fileSystemParser(lines []string, dir *Directory) *Directory {
@@ -69,4 +79,20 @@ func getDirSumBelowSize(dir *Directory, max int) int {
 		total += getDirSumBelowSize(subDir, max)
 	}
 	return total
+}
+
+func getAllEligibleSubDir(dir *Directory, spaceNeeded int) []*Directory {
+	dirs := make([]*Directory, 0)
+	if dir.size > spaceNeeded {
+		dirs = append(dirs, dir)
+	}
+
+	for _, subDir := range dir.directories {
+		if subDir.size < spaceNeeded {
+			continue
+		}
+		dirs = append(dirs, getAllEligibleSubDir(subDir, spaceNeeded)...)
+	}
+
+	return dirs
 }
