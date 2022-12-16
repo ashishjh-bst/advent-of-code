eac9b14dab2208ef1e28c54ed4f52e13a28f87ec
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2022-12-16 14:25:47 +0530

    fixed day 13 part 1

--- day13/solution.go
+++ day13/solution.go
@@ -14,15 +14,13 @@ func Part1(input *string) string {
 		valid := true
 		leftSignal := lines[i]
 		rightSignal := lines[i+1]
-		fmt.Printf("\n PARSING %s and %s", leftSignal, rightSignal)
 		valid = comparator(leftSignal, rightSignal)
-		fmt.Printf("\n IS IT VALID? %t", valid)
 		if valid {
 			validSignals = append(validSignals, signalCounter)
 		}
 		signalCounter++
 	}
-	fmt.Printf("\n %v", validSignals)
+
 	result := 0
 	for _, item := range validSignals {
 		result += item
@@ -39,36 +37,30 @@ func isInt(v interface{}) bool {
 }
 
 func comparator(left string, right string) bool {
-	fmt.Print("\nNOW PARSING LEFT")
 	leftQ, _ := parse(left)
-	fmt.Printf("\n %v", leftQ)
-	fmt.Print("\nNOW PARSING RIGHT")
 	rightQ, _ := parse(right)
-	fmt.Printf("\n %v", rightQ)
 	leftQ = leftQ[0].([]interface{})
 	rightQ = rightQ[0].([]interface{})
-	fmt.Printf("\n COMPARING %v %v", leftQ, rightQ)
 	for i := 0; i < len(leftQ); i++ {
 		if i >= len(rightQ) {
 			return false
 		}
 		comparison := compare(leftQ[i], rightQ[i])
-		fmt.Printf("\n comparison at index %d with left %v  right %v is %d", i, leftQ[i], rightQ[i], comparison)
 		if comparison == 0 {
 			continue
 		}
 		if comparison == -1 {
 			return false
 		}
+		if comparison == 1 {
+			return true
+		}
 	}
-
 	return true
 }
 
 func compare(left interface{}, right interface{}) int {
-	fmt.Printf("\n COMPARING %v %v", left, right)
 	if isInt(left) && isInt(right) {
-		fmt.Printf("\n BOTH %v %v are ints", left, right)
 		l := left.(int)
 		r := right.(int)
 		if l < r {
@@ -79,43 +71,29 @@ func compare(left interface{}, right interface{}) int {
 		}
 		return 0
 	}
-
 	leftQ := make([]interface{}, 0)
 	rightQ := make([]interface{}, 0)
 	if isInt(left) {
-		fmt.Printf("\n left %v is an int, converting to slice", left)
 		leftQ = append(leftQ, left)
 	} else {
-		fmt.Printf("\n left %v is a slice", left)
 		leftQ = left.([]interface{})
 	}
 	if isInt(right) {
-		fmt.Printf("\n right %v is an int, converting to slice", right)
 		rightQ = append(rightQ, right)
 	} else {
-		fmt.Printf("\n right %v is a slice", right)
 		rightQ = right.([]interface{})
 	}
-
-	fmt.Println(len(leftQ), len(rightQ))
-
-	if len(leftQ) == 0 && len(rightQ) > 0 {
-		return 1
-	}
-
-	if len(leftQ) > 0 && len(rightQ) == 0 {
-		return -1
-	}
-
 	for i := 0; i < len(leftQ); i++ {
 		if i >= len(rightQ) {
-			return 1
+			return -1
 		}
 		comparison := compare(leftQ[i], rightQ[i])
-		fmt.Printf("\n comparison at index %d with left %v  right %v is %d", i, leftQ[i], rightQ[i], comparison)
 		if comparison != 0 {
 			return comparison
 		}
+	}
+	if len(leftQ) < len(rightQ) {
+		return 1
 	}
 	return 0
 }
