61bdcaa7706b7cf328efc1c5f7c65ac72e8250bf
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2023-12-06 18:28:26 +0530

    added 2015 day 2

--- y2015/day2/solutions.go
+++ y2015/day2/solutions.go
@@ -1,9 +1,32 @@
 package day2
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Part1(input *string) string {
-	return ""
+	gifts := strings.Split(*input, "\n")
+	sum := 0
+	for _, gift := range gifts {
+		sides := strings.Split(gift, "x")
+		l, _ := strconv.Atoi(sides[0])
+		b, _ := strconv.Atoi(sides[1])
+		h, _ := strconv.Atoi(sides[2])
+		sum += 2*(l*b+b*h+h*l) + min(l*b, b*h, h*l)
+	}
+	return strconv.Itoa(sum)
 }
 
 func Part2(input *string) string {
-	return ""
+	gifts := strings.Split(*input, "\n")
+	sum := 0
+	for _, gift := range gifts {
+		sides := strings.Split(gift, "x")
+		l, _ := strconv.Atoi(sides[0])
+		b, _ := strconv.Atoi(sides[1])
+		h, _ := strconv.Atoi(sides[2])
+		sum += 2*min(l+b, l+h, b+h) + (l * b * h)
+	}
+	return strconv.Itoa(sum)
 }
