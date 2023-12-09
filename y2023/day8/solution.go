6b6fd1d32cc4b8c0ed4d267f373cc3502bd122e4
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2023-12-09 06:48:05 +0530

    added solution for day8 part2

--- y2023/day8/solution.go
+++ y2023/day8/solution.go
@@ -1,6 +1,7 @@
 package day8
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -28,6 +29,22 @@ func (nm *NodeMap) TraverseInstructions(instruction string, startNode string, co
 	return nm.TraverseInstructions(instruction, startNode, count)
 }
 
+func (nm *NodeMap) TraverseInstructions2(instruction string, startNode string, count int) int {
+	for _, i := range instruction {
+		nextNode := (*nm)[startNode]
+		if i == 'L' {
+			startNode = nextNode.left
+		} else {
+			startNode = nextNode.right
+		}
+		count++
+	}
+	if startNode[2:] == "Z" {
+		return count
+	}
+	return nm.TraverseInstructions2(instruction, startNode, count)
+}
+
 func parseNode(data string) Node {
 	nodes := strings.Split(data, ",")
 	node := &Node{}
@@ -49,5 +66,50 @@ func Part1(input *string) string {
 }
 
 func Part2(input *string) string {
-	return ""
+	nodeMap := make(NodeMap)
+	data := strings.Split(*input, "\n\n")
+	instructions := data[0]
+	var startNodes []string
+	for _, line := range strings.Split(data[1], "\n") {
+		nodeData := strings.Split(line, "=")
+		node := strings.Trim(nodeData[0], " ")
+		nodeMap[node] = parseNode(nodeData[1])
+		if node[2:] == "A" {
+			startNodes = append(startNodes, node)
+		}
+	}
+	var nodeCounters []int
+	fmt.Printf("%#v", startNodes)
+	for _, node := range startNodes {
+		count := nodeMap.TraverseInstructions2(instructions, node, 0)
+		nodeCounters = append(nodeCounters, count)
+	}
+	fmt.Printf("%#v", nodeCounters)
+	var lcm int
+	if len(nodeCounters) == 2 {
+		lcm = LCM(nodeCounters[0], nodeCounters[1])
+	} else {
+		lcm = LCM(nodeCounters[0], nodeCounters[1], nodeCounters[2:]...)
+	}
+
+	return strconv.Itoa(lcm)
+}
+
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
