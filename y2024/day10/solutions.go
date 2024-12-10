5b480a7ed04aa6f6c0dcbc0d344bcea50c7bfe77
Ashish <ashishjh-bst@users.noreply.github.com>
2024-12-10 14:25:07 +0530

    clean up

--- y2024/day10/solutions.go
+++ y2024/day10/solutions.go
@@ -12,7 +12,7 @@ func Part1(input *string) string {
 	grid, starts := makeGrid(input)
 	var totalTrailHeads int
 	for _, start := range *starts {
-		totalTrailHeads += countTrailFromStart(grid, start)
+		totalTrailHeads += countTrailEnds(grid, start)
 	}
 	return fmt.Sprintf("%d", totalTrailHeads)
 }
@@ -22,7 +22,7 @@ func Part2(input *string) string {
 	tEnds := &map[[2]int]int{}
 	for _, start := range *starts {
 		for _, dir := range Directions {
-			isTrail(grid, start, dir, tEnds)
+			traverseTrail(grid, start, dir, tEnds)
 		}
 	}
 	var totalRating int
@@ -52,34 +52,31 @@ func makeGrid(input *string) (*[][]int, *[][2]int) {
 	return &grid, &starts
 }
 
-func countTrailFromStart(grid *[][]int, start [2]int) int {
+func countTrailEnds(grid *[][]int, start [2]int) int {
 	tEnds := make(map[[2]int]int)
 	for _, dir := range Directions {
-		isTrail(grid, start, dir, &tEnds)
+		traverseTrail(grid, start, dir, &tEnds)
 	}
 	return len(tEnds)
 }
 
-func isTrail(grid *[][]int, current [2]int, dir [2]int, tEnds *map[[2]int]int) bool {
+func traverseTrail(grid *[][]int, current [2]int, dir [2]int, tEnds *map[[2]int]int) {
 	currentValue := (*grid)[current[0]][current[1]]
 	next := [2]int{current[0] + dir[0], current[1] + dir[1]}
 	if isPosOutside(next, grid) {
-		return false
+		return
 	}
 	nextValue := (*grid)[next[0]][next[1]]
 	if nextValue != currentValue+1 {
-		return false
+		return
 	}
 	if nextValue == 9 {
 		(*tEnds)[next]++
-		return true
+		return
 	}
-	trail := false
 	for _, dir := range Directions {
-		isNextTrail := isTrail(grid, next, dir, tEnds)
-		trail = trail || isNextTrail
+		traverseTrail(grid, next, dir, tEnds)
 	}
-	return trail
 }
 
 func isPosOutside(currentPos [2]int, grid *[][]int) bool {
