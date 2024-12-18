10623aebb8714e261d604fed5e99aa99a4485bd0
Ashish <ashishjh-bst@users.noreply.github.com>
2024-12-19 01:02:18 +0530

    clean up

--- y2024/day15/solutions.go
+++ y2024/day15/solutions.go
@@ -8,59 +8,45 @@ import (
 type Grid [][]string
 
 func Part1(input *string) string {
-	grid, moves, pos := parseInput(input)
-	//grid.print()
+	grid, moves, pos := parseInput(input, false)
 	for _, move := range *moves {
 		dir := getDir(move)
-		hasMoved := grid.move(pos, dir)
-		if hasMoved {
-			pos = [2]int{pos[0] + dir[0], pos[1] + dir[1]}
+		if grid.move(pos, dir) {
+			pos = getNext(pos, dir)
 		}
-		// grid.print()
 	}
-	return fmt.Sprintf("%d", grid.getBoxSum())
+	return fmt.Sprintf("%d", grid.getBoxSum("O"))
 }
 
 func Part2(input *string) string {
-	grid, moves, pos := parseInput2(input)
-	//grid.print()
+	grid, moves, pos := parseInput(input, true)
 	for _, move := range *moves {
 		dir := getDir(move)
-		//fmt.Printf("\nPos:%v ,Move: %s, dir: %v", pos, string(move), dir)
-		hasMoved := grid.move2(pos, move)
-
-		//fmt.Printf(" hasMoved: %t", hasMoved)
-		if hasMoved {
-			pos = [2]int{pos[0] + dir[0], pos[1] + dir[1]}
+		if grid.move2(pos, move) {
+			pos = getNext(pos, dir)
 		}
-		//grid.print()
 	}
-	return fmt.Sprintf("%d", grid.getBigBoxSum())
+	return fmt.Sprintf("%d", grid.getBoxSum("["))
 }
 
-func parseInput(input *string) (*Grid, *string, [2]int) {
+func parseInput(input *string, big bool) (*Grid, *string, [2]int) {
 	values := strings.Split(*input, "\n\n")
+	if big {
+		grid, start := makeBigGrid(&values[0])
+		return grid, &values[1], start
+	}
 	grid, start := makeGrid(&values[0])
-	moves := &values[1]
-	return grid, moves, start
-}
-
-func parseInput2(input *string) (*Grid, *string, [2]int) {
-	values := strings.Split(*input, "\n\n")
-	grid, start := makeBigGrid(&values[0])
-	moves := &values[1]
-	return grid, moves, start
+	return grid, &values[1], start
 }
 
 func makeGrid(input *string) (*Grid, [2]int) {
-	grid := make(Grid, 0)
 	lines := strings.Split(*input, "\n")
+	grid := make(Grid, len(lines))
 	var pos [2]int
 	for i, line := range lines {
 		cols := strings.Split(line, "")
-		grid = append(grid, cols)
-		j := strings.Index(line, "@")
-		if j != -1 {
+		grid[i] = cols
+		if j := strings.Index(line, "@"); j != -1 {
 			pos = [2]int{i, j}
 		}
 	}
@@ -68,41 +54,32 @@ func makeGrid(input *string) (*Grid, [2]int) {
 }
 
 func makeBigGrid(input *string) (*Grid, [2]int) {
-	grid := make(Grid, 0)
 	lines := strings.Split(*input, "\n")
+	grid := make(Grid, len(lines))
 	var pos [2]int
 	for i, line := range lines {
-		cols := strings.Split(line, "")
-		widthCounter := 0
-		row := make([]string, 0)
-		for _, col := range cols {
+		row := make([]string, 0, len(line)*2)
+		for j, col := range line {
 			switch col {
-			case "#":
+			case '#':
 				row = append(row, "#", "#")
-			case "O":
+			case 'O':
 				row = append(row, "[", "]")
-			case ".":
+			case '.':
 				row = append(row, ".", ".")
-			case "@":
+			case '@':
 				row = append(row, "@", ".")
-				pos = [2]int{i, widthCounter}
+				pos = [2]int{i, j * 2}
 			}
-			widthCounter += 2
 		}
-		grid = append(grid, row)
+		grid[i] = row
 	}
 	return &grid, pos
 }
 
 func (g *Grid) move(pos [2]int, dir [2]int) bool {
-	next := [2]int{pos[0] + dir[0], pos[1] + dir[1]}
-	if next == pos {
-		return false
-	}
-	if g.isOutside(next) {
-		return false
-	}
-	if (*g)[next[0]][next[1]] == "#" {
+	next := getNext(pos, dir)
+	if next == pos || g.isOutside(next) || (*g)[next[0]][next[1]] == "#" {
 		return false
 	}
 	if (*g)[next[0]][next[1]] == "." || g.move(next, dir) {
@@ -116,48 +93,14 @@ func (g *Grid) move(pos [2]int, dir [2]int) bool {
 func (g *Grid) move2(pos [2]int, move rune) bool {
 	dir := getDir(move)
 	next := getNext(pos, dir)
-
 	if g.isBlocked(next) {
 		return false
 	}
-	nextBlock := g.getBlock(next)
-	if nextBlock == "." {
+	if g.getBlock(next) == "." {
 		return g.move(pos, dir)
 	}
-	var box = g.getBox(next)
-	return g.isBoxMovable(box, move) && g.moveBox(box, move) && g.move(pos, dir)
-}
-
-func (g *Grid) isBoxMovableV(box [2][2]int, move rune) bool {
-	dir := getDir(move)
-	nextStart := getNext(box[0], dir)
-	nextEnd := getNext(box[1], dir)
-	if g.isBlocked(nextStart) || g.isBlocked(nextEnd) {
-		return false
-	}
-	nextStartBlock := g.getBlock(nextStart)
-	nextEndBlock := g.getBlock(nextEnd)
-	if nextStartBlock == "." && nextEndBlock == "." {
-		return true
-	}
-	if nextStartBlock+nextEndBlock == "[]" {
-		nextBox := g.getBox(nextStart)
-		return g.isBoxMovableV(nextBox, move)
-	}
-	if nextStartBlock+nextEndBlock == "]." {
-		nextBox := g.getBox([2]int{nextStart[0], nextStart[1] - 1})
-		return g.isBoxMovableV(nextBox, move)
-	}
-	if nextStartBlock+nextEndBlock == ".[" {
-		nextBox := g.getBox([2]int{nextEnd[0], nextEnd[1] + 1})
-		return g.isBoxMovableV(nextBox, move)
-	}
-	if nextStartBlock+nextEndBlock == "][" {
-		box1 := g.getBox([2]int{nextStart[0], nextStart[1] - 1})
-		box2 := g.getBox([2]int{nextEnd[0], nextEnd[1] + 1})
-		return g.isBoxMovableV(box1, move) && g.isBoxMovableV(box2, move)
-	}
-	return false
+	box := g.getBox(next)
+	return g.moveBox(box, move) && g.move(pos, dir)
 }
 
 func (g *Grid) moveBox(box [2][2]int, move rune) bool {
@@ -172,25 +115,22 @@ func (g *Grid) moveBoxH(box [2][2]int, move rune) bool {
 		return false
 	}
 	dir := getDir(move)
-
+	next := getNext(box[1], dir)
 	if move == '>' {
-		next := getNext(box[1], dir)
-		nextBlock := g.getBlock(next)
-		if nextBlock == "." {
+		if g.getBlock(next) == "." {
 			return g.move(box[1], dir) && g.move(box[0], dir)
 		}
-		if nextBlock == "[" {
-			nextBox := g.getBox([2]int{next[0], next[1] + 1})
+		if g.getBlock(next) == "[" {
+			nextBox := g.getBox(getNext(next, dir))
 			return g.moveBox(nextBox, move) && g.move(box[1], dir) && g.move(box[0], dir)
 		}
 	} else if move == '<' {
-		next := getNext(box[0], dir)
-		nextBlock := g.getBlock(next)
-		if nextBlock == "." {
+		next = getNext(box[0], dir)
+		if g.getBlock(next) == "." {
 			return g.move(box[0], dir) && g.move(box[1], dir)
 		}
-		if nextBlock == "]" {
-			nextBox := g.getBox([2]int{next[0], next[1] - 1})
+		if g.getBlock(next) == "]" {
+			nextBox := g.getBox(getNext(next, dir))
 			return g.moveBox(nextBox, move) && g.move(box[0], dir) && g.move(box[1], dir)
 		}
 	}
@@ -199,35 +139,31 @@ func (g *Grid) moveBoxH(box [2][2]int, move rune) bool {
 
 func (g *Grid) isBoxMovableH(box [2][2]int, move rune) bool {
 	dir := getDir(move)
-
+	next := getNext(box[1], dir)
 	if move == '>' {
-		next := getNext(box[1], dir)
 		if g.isBlocked(next) {
 			return false
 		}
-		nextBlock := g.getBlock(next)
-		if nextBlock == "." {
+		if g.getBlock(next) == "." {
 			return true
 		}
-		if nextBlock == "[" {
-			nextBox := g.getBox([2]int{next[0], next[1] + 1})
+		if g.getBlock(next) == "[" {
+			nextBox := g.getBox(getNext(next, dir))
 			return g.isBoxMovableH(nextBox, move)
 		}
 	} else if move == '<' {
-		next := getNext(box[0], dir)
+		next = getNext(box[0], dir)
 		if g.isBlocked(next) {
 			return false
 		}
-		nextBlock := g.getBlock(next)
-		if nextBlock == "." {
+		if g.getBlock(next) == "." {
 			return true
 		}
-		if nextBlock == "]" {
-			nextBox := g.getBox([2]int{next[0], next[1] - 1})
+		if g.getBlock(next) == "]" {
+			nextBox := g.getBox(getNext(next, dir))
 			return g.isBoxMovableH(nextBox, move)
 		}
 	}
-
 	return false
 }
 
@@ -238,36 +174,57 @@ func (g *Grid) moveBoxV(box [2][2]int, move rune) bool {
 	dir := getDir(move)
 	nextStart := getNext(box[0], dir)
 	nextEnd := getNext(box[1], dir)
-	nextStartBlock := g.getBlock(nextStart)
-	nextEndBlock := g.getBlock(nextEnd)
-	if nextStartBlock == "." && nextEndBlock == "." {
+	if g.getBlock(nextStart) == "." && g.getBlock(nextEnd) == "." {
 		return g.move(box[0], dir) && g.move(box[1], dir)
 	}
-	if nextStartBlock+nextEndBlock == "[]" {
+	if g.getBlock(nextStart)+g.getBlock(nextEnd) == "[]" {
 		nextBox := g.getBox(nextStart)
 		return g.isBoxMovableV(nextBox, move) && g.moveBoxV(nextBox, move) && g.move(box[0], dir) && g.move(box[1], dir)
 	}
-	if nextStartBlock+nextEndBlock == "]." {
-		nextBox := g.getBox([2]int{nextStart[0], nextStart[1] - 1})
+	if g.getBlock(nextStart)+g.getBlock(nextEnd) == "]." {
+		nextBox := g.getBox(getNext(nextStart, [2]int{0, -1}))
 		return g.isBoxMovableV(nextBox, move) && g.moveBoxV(nextBox, move) && g.move(box[0], dir) && g.move(box[1], dir)
 	}
-	if nextStartBlock+nextEndBlock == ".[" {
-		nextBox := g.getBox([2]int{nextEnd[0], nextEnd[1] + 1})
+	if g.getBlock(nextStart)+g.getBlock(nextEnd) == ".[" {
+		nextBox := g.getBox(getNext(nextEnd, [2]int{0, 1}))
 		return g.isBoxMovableV(nextBox, move) && g.moveBoxV(nextBox, move) && g.move(box[0], dir) && g.move(box[1], dir)
 	}
-	if nextStartBlock+nextEndBlock == "][" {
-		box1 := g.getBox([2]int{nextStart[0], nextStart[1] - 1})
-		box2 := g.getBox([2]int{nextEnd[0], nextEnd[1] + 1})
+	if g.getBlock(nextStart)+g.getBlock(nextEnd) == "][" {
+		box1 := g.getBox(getNext(nextStart, [2]int{0, -1}))
+		box2 := g.getBox(getNext(nextEnd, [2]int{0, 1}))
 		return g.isBoxMovableV(box1, move) && g.isBoxMovableV(box2, move) && g.moveBoxV(box1, move) && g.moveBoxV(box2, move) && g.move(box[0], dir) && g.move(box[1], dir)
 	}
 	return false
 }
 
-func (g *Grid) isBoxMovable(box [2][2]int, move rune) bool {
-	if move == '<' || move == '>' {
-		return g.isBoxMovableH(box, move)
+func (g *Grid) isBoxMovableV(box [2][2]int, move rune) bool {
+	dir := getDir(move)
+	nextStart := getNext(box[0], dir)
+	nextEnd := getNext(box[1], dir)
+	if g.isBlocked(nextStart) || g.isBlocked(nextEnd) {
+		return false
 	}
-	return g.isBoxMovableV(box, move)
+	if g.getBlock(nextStart) == "." && g.getBlock(nextEnd) == "." {
+		return true
+	}
+	if g.getBlock(nextStart)+g.getBlock(nextEnd) == "[]" {
+		nextBox := g.getBox(nextStart)
+		return g.isBoxMovableV(nextBox, move)
+	}
+	if g.getBlock(nextStart)+g.getBlock(nextEnd) == "]." {
+		nextBox := g.getBox(getNext(nextStart, [2]int{0, -1}))
+		return g.isBoxMovableV(nextBox, move)
+	}
+	if g.getBlock(nextStart)+g.getBlock(nextEnd) == ".[" {
+		nextBox := g.getBox(getNext(nextEnd, [2]int{0, 1}))
+		return g.isBoxMovableV(nextBox, move)
+	}
+	if g.getBlock(nextStart)+g.getBlock(nextEnd) == "][" {
+		box1 := g.getBox(getNext(nextStart, [2]int{0, -1}))
+		box2 := g.getBox(getNext(nextEnd, [2]int{0, 1}))
+		return g.isBoxMovableV(box1, move) && g.isBoxMovableV(box2, move)
+	}
+	return false
 }
 
 func (g *Grid) getBlock(pos [2]int) string {
@@ -275,13 +232,7 @@ func (g *Grid) getBlock(pos [2]int) string {
 }
 
 func (g *Grid) isBlocked(pos [2]int) bool {
-	if g.isOutside(pos) {
-		return true
-	}
-	if g.getBlock(pos) == "#" {
-		return true
-	}
-	return false
+	return g.isOutside(pos) || g.getBlock(pos) == "#"
 }
 
 func getNext(pos [2]int, dir [2]int) [2]int {
@@ -289,10 +240,7 @@ func getNext(pos [2]int, dir [2]int) [2]int {
 }
 
 func (g *Grid) isOutside(pos [2]int) bool {
-	if pos[0] < 0 || pos[0] >= len(*g) || pos[1] < 0 || pos[1] >= len((*g)[0]) {
-		return true
-	}
-	return false
+	return pos[0] < 0 || pos[0] >= len(*g) || pos[1] < 0 || pos[1] >= len((*g)[0])
 }
 
 func (g *Grid) getBox(pos [2]int) [2][2]int {
@@ -302,29 +250,11 @@ func (g *Grid) getBox(pos [2]int) [2][2]int {
 	return [2][2]int{{pos[0], pos[1] - 1}, pos}
 }
 
-func (g *Grid) print() {
-	for _, row := range *g {
-		fmt.Printf("\n %v", row)
-	}
-}
-
-func (g *Grid) getBoxSum() int {
+func (g *Grid) getBoxSum(boxType string) int {
 	var sum int
 	for i, row := range *g {
 		for j, col := range row {
-			if col == "O" {
-				sum += i*100 + j
-			}
-		}
-	}
-	return sum
-}
-
-func (g *Grid) getBigBoxSum() int {
-	var sum int
-	for i, row := range *g {
-		for j, col := range row {
-			if col == "[" {
+			if col == boxType {
 				sum += i*100 + j
 			}
 		}
